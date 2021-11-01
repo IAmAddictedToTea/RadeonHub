1c366cb5fe9729d765d4c514d2f1f039c0d008d7
IAmAddictedToTea <90111537+IAmAddictedToTea@users.noreply.github.com>
2021-11-01 12:40:31 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,1 @@
-local localplayer = game:GetService("Players").LocalPlayer
-local blacklisted = {"2821579391"}
-
-for _, blacklisted in pairs(blacklisted) do
-	if localplayer.UserId == blacklisted then
-		localplayer:Kick("You are not allowed to use this script!")
-	else
-		print("Sweaty was here :)")
-	end
-end
+print("Sweaty was here :)")
