f5f1f417bf3fdbbf63b0d2211fcd83d292891953
IAmAddictedToTea <90111537+IAmAddictedToTea@users.noreply.github.com>
2021-11-02 16:47:51 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
-print("Sweaty was here :)")
+local blacklisted = {"2821579391"}
+local localplr = game:GetService("Players").LocalPlayer
+
+for _, blacklisted in pairs(blacklisted) do
+    if localplr.UserId == blacklisted then
+        localplr:Kick("You are not allowed to use RadeonHub. fuck off.")
+    end
+end
