70741505fa97a573aff04f6a1f18faac528fb434
IAmAddictedToTea <90111537+IAmAddictedToTea@users.noreply.github.com>
2021-11-03 06:51:30 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,6 +8,10 @@ local plr = game:GetService("Players").LocalPlayer
 for _, v in pairs(blacklisted) do
 	if plr.UserId == v then
 		plr:Kick("You are not allowed to use RadeonHub.")
+	else
+		if plr.AccountAge >= 5 then
+			plr:Kick("Your account must be at least 5 days old to use RadeonHub.")
+		end
 	end
 end
 
@@ -15,10 +19,10 @@ print("Sweaty was here.")
 
 --DBOG Script.
 function dbog()
-    print("Dbog") -- Placeholder. Actual script code will be put here once I am done.
+	print("Dbog") -- Placeholder. Actual script code will be put here once I am done.
 end
 
---Looking for game. It is in a weird order due to functions needed to be specified before they can be called.
+--Looking for game.
 if game.CreatorId == 5237711 then
-    dbog()
+	dbog()
 end
